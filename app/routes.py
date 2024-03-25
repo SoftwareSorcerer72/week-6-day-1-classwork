45389c5fcaba408d487e8176dabf17d3b7f8ba28
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-03-25 15:27:36 -0400

    fisished setting up two routes; get all posts and get single posts

--- app/routes.py
+++ app/routes.py
@@ -17,3 +17,18 @@ def get_posts():
     # Get the posts from storage (fake data -> tomorrow will be db)
     posts = post_data 
     return posts
+
+
+# Get a Single Post By ID
+@app.route('/posts/<int:post_id>')
+def get_post(post_id):
+    # Get the posts from storage
+    posts = post_data
+    # For each dictionary in the list of post dictionaries
+    for post in posts:
+        # If the key of 'id' matches the post_id from the URL
+        if post['id'] == post_id:
+            # Return that post dictionary
+            return post
+    # If we loop through all of the posts without returning, the post with that ID does not exist
+    return {'error': f"Post with an ID of {post_id} does not exist"}, 404
