45e7406098d31e1679f0f9307bae104faf7682d6
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-03-25 13:11:01 -0400

    Added a second route

--- app.py
+++ app.py
@@ -11,3 +11,16 @@ def index():
     first_name = 'Brian'
     age = 55
     return 'Hello ' +first_name + ' who is ' + str(age) + 'years old'
+
+@app.route('/test')
+def test():
+    my_dicts = []
+    for i in range(5):
+        a_dict = {
+            'id': i+1,
+            'key': 'value',
+            'name': 'Brian'
+        }
+        my_dicts.append(a_dict)
+        
+    return my_dicts, 
